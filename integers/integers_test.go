567edfc878b799ce18b77b57b20d43d07c554db0
Amogh N Rao <amogh090502@gmail.com>
2026-02-20 14:40:38 +0000

    go integers

---
+++ integers/integers_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	fmt.Println(Add(2, 2))
+	// Output: 4
+}
